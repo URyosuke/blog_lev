db9224fc08deb19a022ce2210eedff6fce6bfa8c
URyosuke <kazusa7221@gmail.com>
2023-02-08 10:37:55 +0900

    投稿編集

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,8 +11,7 @@ class PostController extends Controller
     {
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
     }
-
-
+    
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
@@ -27,6 +26,19 @@ class PostController extends Controller
     {
         $input = $request['post'];
         $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+    
         return redirect('/posts/' . $post->id);
     }
 }
